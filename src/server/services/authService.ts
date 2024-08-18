bbf50789c584c66d8c2b425b08cc47db7c54b1d1
luizph27 <luizphelipecirinorodrigues@gmail.com>
2024-08-18 02:14:02 -0300

    feat(login): criação da rota e service de login

--- src/server/services/authService.ts
+++ src/server/services/authService.ts
@@ -1,10 +1,35 @@
 import { prisma } from '../../prisma/prismaClient'
 import bcryptjs from 'bcryptjs'
-
+import { sign } from 'jsonwebtoken'
 export class AuthService {
   async login(usuario: any) {
-    const user = await prisma.usuario.findUnique({ where: { nome: usuario.nome } })
-    const isValid = await bcryptjs.compare(usuario.senha, user.senha)
-    return isValid
+    try {
+      const user = await prisma.usuario.findFirst({
+        where: { nome: usuario.nome, AND: { ativo: true } },
+        select: { senha: true, id: true, nome: true },
+      })
+
+      if (!user) {
+        throw new Error('Usuário não cadastrado ou inativo')
+      }
+
+      const { senha, id, nome } = user
+
+      const isValid = await bcryptjs.compare(usuario.senha, senha)
+
+      if (!isValid) {
+        throw new Error("As senhas não conferem")
+      }
+      const accessToken = sign({ 
+        id: id, nome: nome
+       }, process.env.SECRET_KEY, { 
+        expiresIn: 3000 
+      })
+
+      return { id, nome, accessToken }
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
   }
 }
